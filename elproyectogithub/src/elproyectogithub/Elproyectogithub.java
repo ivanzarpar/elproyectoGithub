2736ca538a5d421b6613beecaa5ddf6fc56f745d
ivan_ <ivan_@LAPTOP-LVTM11G4>
2019-12-01 16:37:31 +0100

    inicio proyecto

---
+++ elproyectogithub/src/elproyectogithub/Elproyectogithub.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package elproyectogithub;
+
+/**
+ *
+ * @author ivan_
+ */
+public class Elproyectogithub {
+String nombre = " ";
+int notadeseada = 0;
+int notareal = 0;
+
+public static String veredicto(String valor1, String valor2) {
+
+
+
+        if (valor1.equals(valor2)) {
+
+            return "Perfecto. Has tenido buen criterio.";
+
+        }
+
+        if (Integer.valueOf(valor1) < Integer.valueOf(valor2)) {
+
+            return "Has mejorado, o te ha llegado el éxito inesperado.";
+
+        }
+
+        return "Te has confiado. Falta realismo.";
+
+    }
+    
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("La calificación de " + args[0] + " : ");
+    }
+    
+}
