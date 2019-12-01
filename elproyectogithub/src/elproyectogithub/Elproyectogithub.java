a41800f6efdd19ae699890730439f13fe6c0a811
ivan_ <ivan_@LAPTOP-LVTM11G4>
2019-12-01 17:05:21 +0100

    Cambios

--- elproyectogithub/src/elproyectogithub/Elproyectogithub.java
+++ elproyectogithub/src/elproyectogithub/Elproyectogithub.java
@@ -16,22 +16,16 @@ private int notareal = 0;
 
 public static String veredicto(String valor1, String valor2) {
 /*Según los valores introducidos, el programa nos devolverá un resultado*/
-        if (valor1.equals(valor2)) {
+int notadeseada = 0;
+int notareal= 0;
 
+if (notadeseada == notareal){
             return "Perfecto. Has tenido buen criterio.";
-
         }
-
-        if (Integer.valueOf(valor1) < Integer.valueOf(valor2)) {
-
+        if (notadeseada > notareal){
             return "Has mejorado, o te ha llegado el éxito inesperado.";
-
         }
-
         return "Te has confiado. Falta realismo.";
-
-    
-    
             }
     
     /**
@@ -40,6 +34,7 @@ public static String veredicto(String valor1, String valor2) {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("La calificación de " + args[0] + " : ");
+        
     }
     
 }
